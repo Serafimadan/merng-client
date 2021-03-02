2044c155aabb84cd434c3563893d4a2f1bc4ab53
serafima <svp1982@mail.ru>
2021-03-02 14:40:59 +0100

    Init

---
+++ src/util/graphql.js
@@ -0,0 +1,17 @@
+import gql from 'graphql-tag';
+
+export const FETCH_POSTS_QUERY = gql`
+{
+    getPosts{
+        id body createdAt username 
+        likeCount
+        likes{
+            id username createdAt
+        }
+        commentCount
+        comments{
+            id username createdAt body
+        }
+    }
+}
+`
---
+++ src/util/hooks.js
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+
+export const useForm = (callback, initialState = {}) => {
+    const [values, setValues] = useState(initialState);
+
+    const onChange = async (e) => {
+        try{  
+            await setValues({...values, [e.target.name] : e.target.value})
+        } catch(e) {
+            console.error(e);
+        }
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try { 
+            await callback();
+        } catch(e) {
+            console.error(e);
+        }
+    };
+    return {
+        onChange,
+        onSubmit,
+        values, 
+    }
+
+}
